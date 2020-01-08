0d3ae1c1de60547aed887ffbd7e0164217a3ddb2
David Janda <djanda97@gmail.com>
2020-01-07 22:24:30 -0800

    Format table rows as a list of lists where the inner list elements contain table data

--- scraper.py
+++ scraper.py
@@ -1,22 +1,39 @@
+# Web scraper to gather data from a wiki page about enemies from Super Metroid.
+# Implemented using the BeautifulSoup and requests libraries.
 from bs4 import BeautifulSoup
+import pprint
 import requests
 
-url = "https://wiki.supermetroid.run/Enemies"
-req = requests.get(url)
+baseUrl = "https://wiki.supermetroid.run"
+enemiesUrl = baseUrl + "/Enemies"
+req = requests.get(enemiesUrl)
+
+pp = pprint.PrettyPrinter(indent=4)
 
 soup = BeautifulSoup(req.text, "html.parser")
 
-# print(soup.prettify())
-
 all_tables = soup.find_all("table")
+crateria = all_tables[0]
 
-# print(all_tables[0])
 print("len(all_tables):", len(all_tables))
 
 table_headings = []
 
-for table in all_tables:
-    table_headings.append(table.find("thead"))
-    print("table:\n", table)
+for th in crateria.find_all("th"):
+    table_headings.append(th.string)
 
-print("table_headings:\n", table_headings)
+print("table_headings:")
+pp.pprint(table_headings)
+
+table_rows = []
+
+# Splicing [1:] ignores first element of list
+for tr in crateria.find_all("tr")[1:]:
+    row = []
+    for td in tr.find_all("td"):
+        # print(td)
+        row.append(td.string)
+    table_rows.append(row)
+
+print("table_rows:")
+pp.pprint(table_rows)
