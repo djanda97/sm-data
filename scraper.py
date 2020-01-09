a74b7152554694ea7cd7a2136d5d2e5d631a9599
David Janda <djanda97@gmail.com>
2020-01-08 23:23:25 -0800

    Extracts image url and enemy name from links

--- scraper.py
+++ scraper.py
@@ -11,8 +11,6 @@ def get_html(filename, url):
         file = open(filename, "r")
         return BeautifulSoup(file, "html.parser")
     else:
-        # baseUrl = "https://wiki.supermetroid.run"
-        # enemiesUrl = baseUrl + "/Enemies"
         req = requests.get(url)
         file = open(filename, "w+")
         file.write(req.text)
@@ -20,21 +18,40 @@ def get_html(filename, url):
 
 
 def get_table_rows(table):
-    # Splicing [1:] ignores first element of list
-    # for tr in table.find_all("tr")[1:]:
     table_rows = [[th.string for th in table.find_all("th")]]
+    baseUrl = "https://wiki.supermetroid.run"
+
+    # for tr in table.find_all("tr"):
+    #     row = [td.string for td in tr.find_all("td")]
+    #     table_rows.append(row)
 
     for tr in table.find_all("tr"):
-        row = [td.string for td in tr.find_all("td")]
+        row = []
+
+        for td in tr.find_all("td"):
+            image = td.find("img")
+            name = td.find_all("a")
+
+            if image:
+                image = image["src"]
+                row.append(baseUrl + image)
+            elif name:
+                name = name[0]
+                row.append(name.string)
+            else:
+                row.append(td.string)
+
         table_rows.append(row)
 
     return [row for row in table_rows if row]
 
 
+# Refactor to not mutate table state
 def assign_data_to_tables(tables, html_tables):
     for i, name in enumerate(tables.keys()):
         table = html_tables[i]
         tables[name] = get_table_rows(table)
+
 
 def main():
     pp = pprint.PrettyPrinter(indent=4)
@@ -45,7 +62,7 @@ def main():
                    "Norfair", "Tourian", "Bosses", "Bosses (Projectiles)")
 
     tables = {name: [] for name in table_names}
-    
+
     assign_data_to_tables(tables, html_tables)
 
     print("\ntables:")
